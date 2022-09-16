d8bfcda98ca85d3d97dc95999c80a5f41c0dc1ee
IemetsO <oksana.v.iemets@gmail.com>
2022-09-16 15:50:14 +0300

    add logout

--- models/contact.js
+++ models/contact.js
@@ -1,53 +1,56 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 const Joi = require("joi");
-const {handleSchemaValidationErrors} = require("../helpers")
+const { handleSchemaValidationErrors } = require("../helpers");
 
-const contactSchema = new Schema({
+const contactSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Set name for contact'],
-        unique: true,
+      type: String,
+      required: [true, "Set name for contact"],
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     favorite: {
-        type: Boolean,
-        default: false,
-    }
-}, {versionKey: false, timestamps: true});
-
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const addSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
-    favorite: Joi.bool(),
-  });
-
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool(),
+});
 
 const updateFavoriteSchema = Joi.object({
-    favorite: Joi.bool(). required(),
-})
+  favorite: Joi.bool().required(),
+});
 
 const schemas = {
-    addSchema,
-    updateFavoriteSchema,
-}
+  addSchema,
+  updateFavoriteSchema,
+};
 
 const Contact = model("contact", contactSchema);
 
-
-
-contactSchema.post("save", handleSchemaValidationErrors)
+contactSchema.post("save", handleSchemaValidationErrors);
 
 module.exports = {
-    schemas,
-    Contact,
-  }
-
+  schemas,
+  Contact,
+};
